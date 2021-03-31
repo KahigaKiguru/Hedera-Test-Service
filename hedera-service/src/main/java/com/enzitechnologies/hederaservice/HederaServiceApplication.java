cfeff557adae020584001d7b0cda52c46f3dd472
KahigaKiguru <dev.enzitechnologies@gmail.com>
2021-03-31 14:40:13 +0300

    initial commit

---
+++ hedera-service/src/main/java/com/enzitechnologies/hederaservice/HederaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.enzitechnologies.hederaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HederaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HederaServiceApplication.class, args);
+	}
+
+}
---
+++ hedera-service/src/test/java/com/enzitechnologies/hederaservice/HederaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enzitechnologies.hederaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HederaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
